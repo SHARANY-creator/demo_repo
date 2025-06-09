7bd7775ea57f360db5d9ab42e1dc336e6c232d3b
SHARANY-creator <microgenesis.k.k@mercedes-benz.com>
2025-06-09 13:26:56 +0530

    Delete expose_server.py

--- expose_server.py
+++
@@ -1,8 +0,0 @@
-from pyngrok import ngrok
-
-# Expose the local port 8000 to the internet
-public_url = ngrok.connect(8000)
-print(f"Public URL: {public_url}")
-
-# Keep the program running to maintain the tunnel
-input("Press Enter to exit...\n")
