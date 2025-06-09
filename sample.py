dd2bec9ee8d7a1a110e7caea7f13b13c738726f0
SHARANY-creator <microgenesis.k.k@mercedes-benz.com>
2025-06-09 13:26:06 +0530

    Delete sample.py

--- sample.py
+++
@@ -1,7 +0,0 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-
-PORT = 8000
-handler = SimpleHTTPRequestHandler
-httpd = HTTPServer(("localhost", PORT), handler)
-print(f"Serving at http://localhost:{PORT}")
-httpd.serve_forever()
