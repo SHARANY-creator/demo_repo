85b9d2e1b5a19f122603df9acf1196df5dd8fbb1
SHARANY-creator <microgenesis.k.k@mercedes-benz.com>
2025-06-11 11:27:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from pydantic import BaseModel
 app = FastAPI()
 
 # Configure CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # You can restrict this in production
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+#app.add_middleware(
+    #CORSMiddleware,
+    #allow_origins=["*"],  # You can restrict this in production
+    #allow_credentials=True,
+    #allow_methods=["*"],
+    #allow_headers=["*"],
 )
 
 # Set up logging
